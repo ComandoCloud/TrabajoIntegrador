c5a5dc552a4711cc667a34429f61a19f3000d9d5
Irene <Irene@DESKTOP-JQEUHD3>
2021-04-29 18:24:57 -0300

    Mi primer comentario que EMOCIÓN! Hice las clases Canchas, Deportes,  ReservasEstados, me faltan los setters y getters to by continue...

---
+++ src/main/java/com/comandocloud/tpintegrador/Canchas.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.comandocloud.tpintegrador;
+
+/**
+ *
+ * @author Irene
+ */
+public class Canchas {
+    private int id_canchas;
+    private int id_deportes;
+    private String descripcion;
+    private String ancho;
+    private String largo;
+    private int borrado;
+}
---
+++ src/main/java/com/comandocloud/tpintegrador/Deportes.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.comandocloud.tpintegrador;
+
+/**
+ *
+ * @author Irene
+ */
+public class Deportes {
+    private int deportes;
+    private String descripcion;
+    private int borrado;  
+}
