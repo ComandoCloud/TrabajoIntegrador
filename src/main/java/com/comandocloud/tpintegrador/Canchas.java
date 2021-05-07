61744e08c89dd9f48f3d73251dcde329d6db3535
Irene <Irene@DESKTOP-JQEUHD3>
2021-05-07 17:55:08 -0300

    Agregué los setters y getters de las tablas Canchas, Deportes y Reservas Estados.

--- src/main/java/com/comandocloud/tpintegrador/Canchas.java
+++ src/main/java/com/comandocloud/tpintegrador/Canchas.java
@@ -16,4 +16,54 @@ public class Canchas {
     private String ancho;
     private String largo;
     private int borrado;
+
+    public int getId_canchas() {
+        return id_canchas;
+    }
+
+    public void setId_canchas(int id_canchas) {
+        this.id_canchas = id_canchas;
+    }
+
+    public int getId_deportes() {
+        return id_deportes;
+    }
+
+    public void setId_deportes(int id_deportes) {
+        this.id_deportes = id_deportes;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(String ancho) {
+        this.ancho = ancho;
+    }
+
+    public String getLargo() {
+        return largo;
+    }
+
+    public void setLargo(String largo) {
+        this.largo = largo;
+    }
+
+    public int getBorrado() {
+        return borrado;
+    }
+
+    public void setBorrado(int borrado) {
+        this.borrado = borrado;
+    }
+    
+    
 }
--- src/main/java/com/comandocloud/tpintegrador/ReservasEstados.java
+++ src/main/java/com/comandocloud/tpintegrador/ReservasEstados.java
@@ -13,4 +13,30 @@ public class ReservasEstados {
     private int reser_est;
     private String descripción;
     private int borrado;
+
+    public int getReser_est() {
+        return reser_est;
+    }
+
+    public void setReser_est(int reser_est) {
+        this.reser_est = reser_est;
+    }
+
+    public String getDescripción() {
+        return descripción;
+    }
+
+    public void setDescripción(String descripción) {
+        this.descripción = descripción;
+    }
+
+    public int getBorrado() {
+        return borrado;
+    }
+
+    public void setBorrado(int borrado) {
+        this.borrado = borrado;
+    }
+    
+    
 }
