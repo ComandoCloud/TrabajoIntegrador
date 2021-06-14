28cf8ba702ba7714f846d613d4383c7ad126799c
WamarpeMaximilianoKiss <mkiss@wamarpe.com.ar>
2021-06-13 23:07:17 -0300

    Creacion de interface IOperacionesBasicas

---
+++ src/main/java/CapaNegocios/IOperacionesBasicas.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CapaNegocios;
+
+
+public interface IOperacionesBasicas<T> {
+    public ResponseObject Guardar(T objeto);
+}
