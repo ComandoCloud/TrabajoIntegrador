17f0df4e7edae0575284fe3b98618a9014bdb93b
Irene <Irene@DESKTOP-JQEUHD3>
2021-06-13 22:24:05 -0300

    Comentarios

--- src/main/java/CapaDatos/Conexion.java
+++ src/main/java/CapaDatos/Conexion.java
@@ -1,55 +1,65 @@
 package CapaDatos;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 public class Conexion {
+
     Connection oCon = null;
-    public static final String Url= "jdbc:mysql://179.51.237.45/db_canchas";
-    public static final String Usuario= "root";
+    //PARAMETROS DE CONEXION
+    public static final String Url = "jdbc:mysql://179.51.237.45/db_canchas";
+    public static final String Usuario = "root";
     public static final String Clave = "root2020";
+    //OBJETOS NECESARIOS PARA CONECTARSE A UNA BASE DE DATOS
     public PreparedStatement comando;
     public ResultSet comandoResult;
-    public Conexion(){   
+
+    public Conexion() {
     }
-    
-    public void Conectar() throws SQLException{
-        if(this.oCon != null && this.oCon.isValid(1)){
-            throw new SQLException("Conexion abierta");          
+
+    //METODO PARA CONECTARSE A LA BASE DE DATOS, SIN LA CONEXION ABIERTA NO SE PUEDE EJECUTAR NINGUN COMANDO SQL
+    public void Conectar() throws SQLException {
+        if (this.oCon != null && this.oCon.isValid(1)) {
+            throw new SQLException("Para conectarse a la base de debe cerrar la conexion creada anteriormente.");
         }
-        try 
-        {
-            if(this.oCon == null){
+        try {
+            if (this.oCon == null) {
                 Class.forName("com.mysql.cj.jdbc.Driver");
                 this.oCon = DriverManager.getConnection(this.Url, this.Usuario, this.Clave);
                 System.out.println("Conexion exitosa.");
             }
         } catch (ClassNotFoundException | SQLException e) {
-            throw new SQLException("Error al conectarse con la base de datos:\n"+e.toString());
+            throw new SQLException("Error al conectarse con la base de datos:\n" + e.toString());
         }
     }
-    
-    public void Desconectar() throws SQLException{
-            if(this.oCon.isValid(3000)){
-                this.oCon = null;
-                this.comando=null;
-                this.comandoResult=null;
-            }
+
+    //METODO PARA DESCONECTARSE DE LA BBDD
+    public void Desconectar() throws SQLException {
+        if (this.oCon.isValid(3000)) {
+            this.oCon = null;
+            this.comando = null;
+            this.comandoResult = null;
+        }
     }
-    public void CrearComando(String Comando) throws SQLException{
+
+    //METODO QUE ESTABLECE EL COMANDO SQL AL OBJETO oCon 
+    public void CrearComando(String Comando) throws SQLException {
         this.comando = oCon.prepareStatement(Comando.toString().toLowerCase());
     }
-    public boolean EjecutarComando() throws SQLException{
-       return this.comando.execute();
+
+    //LLEVA A CABO LA CONSULTA ESTABLECIDA EN EL METODO CREAR COMANDO
+    public boolean EjecutarComando() throws SQLException {
+        return this.comando.execute();
     }
-    
-    public DefaultTableModel Tabla() throws SQLException{
+
+    //TRANSFORMA EL RESULTADO DE UNA COSULTA A UN OBETO 
+    public DefaultTableModel Tabla() throws SQLException {
         this.comandoResult = this.comando.executeQuery();
         ResultSetMetaData metaDatos = this.comandoResult.getMetaData();
         DefaultTableModel dt = new DefaultTableModel();
@@ -57,20 +67,21 @@ public class Conexion {
         tabla.setModel(dt);
         int NumColumn = metaDatos.getColumnCount();
         Object[] nombreColumnas = new Object[NumColumn];
-        Object [] fila = new Object[NumColumn];
-        for(int i=0; i<NumColumn; i++)
+        Object[] fila = new Object[NumColumn];
+        //OBTIENE LOS NOMBRES DE LAS COLUMNAS DEL RESULTADO DE LA CONSULTA 
+        for (int i = 0; i < NumColumn; i++) {
             nombreColumnas[i] = metaDatos.getColumnLabel(i + 1);
+        }
+        //SETEA LA COLUMNAS EN OBJETO QUE SE RETORNARA
         dt.setColumnIdentifiers(nombreColumnas);
-        while(this.comandoResult.next()){
-            for (int i=0;i<NumColumn;i++)
-                fila[i] = this.comandoResult.getObject(i+1);
+        //RECORRE LAS FILAS DEVUELTAS POR LA CONSULTA SQL Y LAS AGREGA A EL OBJETO DT 
+        while (this.comandoResult.next()) {
+            for (int i = 0; i < NumColumn; i++) {
+                fila[i] = this.comandoResult.getObject(i + 1);
+            }
             dt.addRow(fila);
         }
-        //Agrego comentario para subir lo nuevo
+        //RETORNO DefaultTableModel CON LAS COLUMNAS Y LAS FILAS DEVUELTAS POR LA CONSULTA
         return dt;
-    }  
-    //FIN SECCION DE CONEXION
- 
-    public static void main(String[] args) throws SQLException {           
     }
 }
--- src/main/java/CapaDatos/statement.java
+++
@@ -1,5 +0,0 @@
-package CapaDatos;
-
-class statement {
-    
-}
